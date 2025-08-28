779d4067d626054c94bd32fde58253bd3348ebd4
aisaa <aisyah.altho38@smk.belajar.id>
2025-08-28 13:49:34 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -9,8 +9,8 @@ assist = st.slider("Jumlah Assist", 0, 20)
 death = st.slider("Jumlah Death", 0, 20)
 turret = st.slider("Jumlah Turret", 0, 20)
 if st.button("Prediksi"):
-	data_baru = pd.DataFrame([[kill,assist,death,turret]],
-	columns=["kill","assist","death","turret"])
-st.success(f"Hasil Prediksi : {loaded_model.predict(data_baru)[0]}")
-st.balloons()
+	data_baru = pd.DataFrame([[kill,assist,death,turret]], columns=["kill","assist","death","turret"])
+	st.success(f"Hasil Prediksi : {loaded_model.predict(data_baru)[0]}")
+	st.balloons()
+
 st.caption("Dibuat dengan :heart: oleh aisa")
